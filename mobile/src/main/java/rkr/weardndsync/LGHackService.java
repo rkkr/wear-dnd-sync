a7050ce2a4a505a3d0e7e057555cc610a530c427
Raimondas Rimkus <narimantukas@gmail.com>
2017-05-09 18:26:30 +0300

    Add missing files

---
+++ mobile/src/main/java/rkr/weardndsync/LGHackService.java
@@ -0,0 +1,11 @@
+package rkr.weardndsync;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class LGHackService extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn){
+    }
+}
---
+++ wear/src/main/java/rkr/weardndsync/TestActivity.java
@@ -0,0 +1,15 @@
+package rkr.weardndsync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        SettingsReceiver.RegisterReceiver(getApplicationContext());
+    }
+}
